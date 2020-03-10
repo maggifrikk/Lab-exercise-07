ef86a7f122ede8068db36f266fc18e3dccf15df4
Magnus Fridrik <magnusfridrik@Magnuss-MacBook-Air.local>
2020-03-10 23:06:06 +0000

    Prints Buzz when only 5 is divisible

--- sample.py
+++ sample.py
@@ -1,3 +1,5 @@
-def FizzBuzz(num):
-    if num % 3 == 0:
+def FizzBuss(num):
+    if num % 5 and num % 3 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0 and num % 3 != 0:
         return "Buzz"
--- test_sample.py
+++ test_sample.py
@@ -3,6 +3,8 @@ import pytest
 def FizzBuss(num):
     if num % 5 and num % 3 == 0:
         return "FizzBuzz"
+    elif num % 5 == 0 and num % 3 != 0:
+        return "Buzz"
 
 def test_FizzBuzz():
-    assert FizzBuss(15) == "Fizzbuzz"
+    assert FizzBuss(20) == "Buzz"
